d5319eea1a9a5794dd3e1a6e5ef738dce3c930df
Jim Morris <morris@wolfman.com>
2019-01-19 11:53:57 +0000

    add more websocekt implementation

--- Firmware/src/modules/utils/network/httpd/httpd.cpp
+++ Firmware/src/modules/utils/network/httpd/httpd.cpp
@@ -6,6 +6,9 @@
 #include "sha1.h"
 #include "base64.h"
 
+#include "OutputStream.h"
+#include "main.h"
+
 #include <string>
 #include <map>
 
@@ -58,11 +61,152 @@ static bool write_page(struct netconn *conn, const char *file)
     return err == ERR_OK;
 }
 
+static err_t websocket_decode(char *buf, uint16_t& len)
+{
+    // TODO handle larger frames
+    unsigned char *data = (unsigned char*) buf;
+    u16_t data_len = len;
+    if (data != NULL && data_len > 2) {
+        if((data[0] & 0x80) == 0) {
+            // must have fin bit
+            printf("websocket_decode: FIN bit not set\n");
+            return ERR_VAL;
+        }
+        if((data[1] & 0x80) == 0) {
+            // must have mask bit
+            printf("websocket_decode: MASK bit not set\n");
+            return ERR_VAL;
+        }
+        uint16_t plen= data[1] & 0x7F;
+        if(plen >= 126 || plen != data_len-6) {
+            printf("websocket_decode: length not < 126 or does not match packet length: %d - %d\n", plen, data_len-6);
+            return ERR_VAL;
+        }
+        uint8_t opcode = data[0] & 0x0F;
+        switch (opcode) {
+            case 0x01: // text
+            case 0x02: // bin
+                if (data_len > 6) {
+                    data_len -= 6;
+                    /* unmask */
+                    for (int i = 0; i < data_len; i++){
+                        data[i + 6] ^= data[2 + i % 4];
+                    }
+                    // decoded data
+                    memmove(buf, &data[6], data_len);
+                    len = data_len;
+                }
+                break;
+            case 0x08: // close
+                return ERR_CLSD;
+                break;
+        }
+        return ERR_OK;
+    }
+    return ERR_VAL;
+}
+
+static int websocket_write(struct netconn *conn, const char *data, uint16_t len)
+{
+    // TODO handle larger packets
+    // TODO handle fragmentation, fragment large packets
+    if (len > 125) {
+        printf("websocket_write: packet too large\n");
+        return len;
+    }
+    unsigned char buf[len + 2];
+    buf[0] = 0x80 | 0x02; // binary
+    buf[1] = len;
+    memcpy(&buf[2], data, len);
+    len += 2;
+    err_t err = netconn_write(conn, buf, len, NETCONN_COPY);
+    if(err != ERR_OK) {
+        printf("websocket_write: error writing: %d\n", err);
+    }
+    return len;
+}
+
+static err_t handle_websocket(struct netconn *conn, const char *keystr)
+{
+    unsigned char encoded_key[32];
+    char key[64];
+    const char WS_GUID[] = "258EAFA5-E914-47DA-95CA-C5AB0DC85B11";
+    int len = strlen(keystr);
+
+    /* Concatenate key */
+    memcpy(key, keystr, len);
+    strcpy(&key[len], WS_GUID); // FIXME needs to be length checked
+    printf("Resulting key: %s\n", key);
+
+    unsigned char sha1sum[20];
+    mbedtls_sha1((unsigned char *) key, sizeof(WS_GUID) + len - 1, sha1sum);
+    /* Base64 encode */
+    unsigned int olen;
+    mbedtls_base64_encode(NULL, 0, &olen, sha1sum, 20); //get length
+    int ok = mbedtls_base64_encode(encoded_key, sizeof(encoded_key), &olen, sha1sum, 20);
+    if (ok == 0) {
+        encoded_key[olen] = '\0';
+        printf("Base64 encoded: %s\n", encoded_key);
+    } else {
+        printf("base64 encode failed\n");
+        return ERR_VAL;
+    }
+
+    /*
+    HTTP/1.1 101 Switching Protocols
+    Upgrade: websocket
+    Connection: Upgrade
+    Sec-WebSocket-Accept: s3pPLMBiTxaQ9kYGzzhZRbK+xOo=
+    */
+    write_header(conn, "HTTP/1.1 101 Switching Protocols\r\n");
+    write_header(conn, "Upgrade: websocket\r\n");
+    write_header(conn, "Connection: Upgrade\r\n");
+    std::string swa("Sec-WebSocket-Accept: ");
+    swa.append((char *)encoded_key);
+    swa.append("\r\n");
+    write_header(conn, swa.c_str());
+    write_header(conn, "\r\n");
+
+    // now the socket is a websocket
+    // ....
+    const u16_t bufsize = 256;
+    char buf[bufsize];
+    char line[132];
+    size_t cnt = 0;
+    bool discard = false;
+    // FIXME this may need to stay around until command thread is done with it
+    OutputStream os([conn](const char *ibuf, size_t ilen) { return websocket_write(conn, ibuf, ilen); });
+    struct pbuf *p;
+    err_t err;
+    // read from connection until it closes
+    while ((err = netconn_recv_tcp_pbuf(conn, &p)) == ERR_OK) {
+        uint16_t n = pbuf_copy_partial(p, buf, bufsize, 0);
+        pbuf_free(p);
+        if(n > 0) {
+            // decode payload, return in buf with new len
+            err = websocket_decode(buf, n);
+            if(err == ERR_OK) {
+                process_command_buffer(n, buf, &os, line, cnt, discard);
+            } else if(err == ERR_CLSD) {
+                break;
+            } else {
+                printf("websocket decode error\n");
+                break;
+            }
+        }
+    }
+
+    // exit string
+    const char ebuf[] = {0x88, 0x02, 0x03, 0xe8};
+    u16_t elen = sizeof(ebuf);
+    return netconn_write(conn, ebuf, elen, NETCONN_COPY);
+}
+
 static err_t readto(struct netconn *conn, struct pbuf* &cp, std::string match, uint16_t& offset, uint16_t from = 0)
 {
-    size_t match_len= match.size();
+    size_t match_len = match.size();
     while(true) {
-        if(cp != NULL && cp->tot_len >= from+match_len) {
+        if(cp != NULL && cp->tot_len >= from + match_len) {
             // find the required character
             u16_t off = pbuf_memfind(cp, match.c_str(), match_len, from);
             if (off != 0xFFFF) {
@@ -91,7 +235,7 @@ static err_t readto(struct netconn *conn, struct pbuf* &cp, std::string match, u
 // for older version of lwip back port these
 u8_t pbuf_remove_header(struct pbuf *p, size_t header_size_decrement)
 {
-     u16_t increment_magnitude;
+    u16_t increment_magnitude;
 
     if ((p == NULL) || (header_size_decrement > 0xFFFF)) {
         return 1;
@@ -103,7 +247,7 @@ u8_t pbuf_remove_header(struct pbuf *p, size_t header_size_decrement)
     increment_magnitude = (u16_t)header_size_decrement;
     /* Check that we aren't going to move off the end of the pbuf */
     if(increment_magnitude > p->len) {
-        increment_magnitude= p->len;
+        increment_magnitude = p->len;
     }
 
     /* increase payload pointer (guarded by length check above) */
@@ -201,11 +345,12 @@ static err_t parse_headers(struct netconn *conn, struct pbuf* &cp, std::string& 
         err = readto(conn, cp, "\r\n", offset, last_offset);
         len = offset - last_offset;
 
+        // if zero then it is a blnk line which indicates end of headers
         if(len == 0) {
             // free up pbufs that have been read
             // NOTE real version of pbuf_free_header() will have issues with last_offset being > pbuf->tot_len
             // So:- if(last_offset > cp->tot_len) last_offset= cp->tot_len;
-            cp = pbuf_free_header(cp, last_offset);
+            cp = pbuf_free_header(cp, offset + 1);
             printf("parse_headers: end of headers\n");
             return ERR_OK;
         }
@@ -217,15 +362,15 @@ static err_t parse_headers(struct netconn *conn, struct pbuf* &cp, std::string& 
             return ERR_VAL;
         }
         hdrbuf[n + 1] = '\0';
-        last_offset= offset+2;
+        last_offset = offset + 2;
 
         printf("parse_headers: reading header: %s\n", hdrbuf);
         char *o = strchr(hdrbuf, ':');
-        if(o == nullptr || o+2 >= &hdrbuf[len]) {
+        if(o == nullptr || o + 2 >= &hdrbuf[len]) {
             printf("parse_headers: bad header\n");
         } else {
-            std::string k(hdrbuf, (size_t)(o-hdrbuf));
-            std::string v(o + 2, (size_t)(len-(o-hdrbuf)-2));
+            std::string k(hdrbuf, (size_t)(o - hdrbuf));
+            std::string v(o + 2, (size_t)(len - (o - hdrbuf) - 2));
             hdrs[k] = v;
         }
     }
@@ -254,46 +399,45 @@ static void http_server_netconn_serve(struct netconn *conn)
             printf(" %s = %s\n", x.first.c_str(), x.second.c_str());
         }
 
-        printf("bytes left in pbuf: %d, cp= %p\n", cp==NULL ? 0 : cp->tot_len, cp);
+        printf("bytes left in pbuf: %d, cp= %p\n", cp == NULL ? 0 : cp->tot_len, cp);
 
         if(cp != NULL) pbuf_free(cp);
 
-        // write test header
-        write_header(conn, http_header_200);
-        write_header(conn, http_content_type_text);
+        /* look for web connect
+            GET / HTTP/1.1
+            Host: server.example.com
+            Upgrade: websocket
+            Connection: Upgrade
+            Sec-WebSocket-Key: dGhlIHNhbXBsZSBub25jZQ==
+        */
 
-        // Send our HTML page
-        write_page(conn, request_target.c_str());
-    }
+        if(method == "GET" && request_target == "/command") {
+            auto i = hdrs.find("Upgrade");
+            if(i != hdrs.end() && i->second == "websocket") {
+                auto k = hdrs.find("Sec-WebSocket-Key");
+                if(k != hdrs.end()) {
+                    std::string key = k->second;
+                    handle_websocket(conn, key.c_str());
+                    return;
+                }
+            }
+            printf("badly formatted websocket request\n");
 
-    // Close the connection (server closes in HTTP)
-    netconn_close(conn);
+        } else if(method == "GET" && request_target == "/") {
+            write_header(conn, http_header_200);
+            write_header(conn, http_content_type_html);
+            write_page(conn, "index.html");
 
-    {
-        unsigned char encoded_key[32];
-        char key[64];
-        const char WS_GUID[] = "258EAFA5-E914-47DA-95CA-C5AB0DC85B11";
-        const char *keystr = "the sample nonce";
-        int len = strlen(keystr);
+        } else if(method == "GET") {
+            write_header(conn, http_header_200);
+            write_header(conn, http_content_type_html);
+            write_page(conn, request_target.c_str());
 
-        /* Concatenate key */
-        memcpy(key, keystr, len);
-        strcpy(&key[len], WS_GUID); // FIXME needs to be length checked
-        printf("Resulting key: %s\n", key);
-
-        unsigned char sha1sum[20];
-        mbedtls_sha1((unsigned char *) key, sizeof(WS_GUID) + len - 1, sha1sum);
-        /* Base64 encode */
-        unsigned int olen;
-        mbedtls_base64_encode(NULL, 0, &olen, sha1sum, 20); //get length
-        int ok = mbedtls_base64_encode(encoded_key, sizeof(encoded_key), &olen, sha1sum, 20);
-        if (ok == 0) {
-            encoded_key[olen] = '\0';
-            printf("Base64 encoded: %s\n", encoded_key);
         } else {
-            printf("base64 encode failed\n");
+            printf("Unhandled request\n");
         }
     }
+
 }
 
 /** The main function, never returns! */
@@ -317,6 +461,8 @@ static void http_server_thread(void *arg)
         err = netconn_accept(conn, &newconn);
         if (err == ERR_OK) {
             http_server_netconn_serve(newconn);
+            // Close the connection (server closes in HTTP)
+            netconn_close(newconn);
             netconn_delete(newconn);
         }
     } while(err == ERR_OK);
