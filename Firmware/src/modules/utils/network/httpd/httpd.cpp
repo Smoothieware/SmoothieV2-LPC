eeb7c6e3710b862ed08c949c5076c8548759aed9
Jim Morris <morris@wolfman.com>
2019-01-20 23:23:08 +0000

    redo error checking

--- Firmware/src/modules/utils/network/httpd/httpd.cpp
+++ Firmware/src/modules/utils/network/httpd/httpd.cpp
@@ -41,7 +41,7 @@
 using hdr_map_t = std::map<std::string, std::string>;
 
 // TODO make this configurabe in config
-static const char *webdir= "/sd/www";
+static const char *webdir = "/sd/www";
 
 /* Send the HTML header
  * NETCONN_NOCOPY: our data is const static, so no need to copy it
@@ -142,7 +142,7 @@ struct pbuf *pbuf_free_header(struct pbuf *q, u16_t size)
 
 // Read data until we have at least len bytes available.
 // conn is the connection
-// cp is a pbuf chain containing reads bytes so far
+// cp is a pbuf chain containing read bytes so far
 // len is the minimum amount of data we need to have read
 // NOTE that there may be excess data left in the pbuf chain
 // usage: one would call pbuf_copy_partial() after this returns to get the len bytes of data
@@ -151,7 +151,7 @@ static err_t read_len_bytes(struct netconn *conn, struct pbuf* &cp, uint16_t len
 {
     while(true) {
         if(cp != NULL && cp->tot_len >= len) {
-           return ERR_OK;
+            return ERR_OK;
         }
 
         // Read more data from the port
@@ -170,21 +170,18 @@ static err_t read_len_bytes(struct netconn *conn, struct pbuf* &cp, uint16_t len
     }
 }
 
-class WebsocketState {
-private:
-    bool in_header{true};
-    bool in_body{false};
-    bool complete{false};
+class WebsocketState
+{
 
 public:
     WebsocketState(struct netconn *c) : conn(c) {}
+    ~WebsocketState() { if(p != NULL) pbuf_free(p); }
+
     struct netconn *conn;
-    struct pbuf *p{nullptr};
+    struct pbuf *p {nullptr};
     uint16_t off{0};
-    bool is_complete() const { return complete; }
-    bool is_header() const { return in_header; }
-    bool is_body() const { return in_body; }
-    void reset() { in_header= true; in_body= false; complete= false;}
+    bool complete{false};
+    void reset() { off = 0; complete = false;}
 };
 
 // Read data from a websocket and decode it.
@@ -197,8 +194,8 @@ static err_t websocket_read(WebsocketState& state, uint8_t *buf, uint16_t buflen
 {
     err_t err;
     if(state.p == nullptr || state.p->tot_len < 6) {
-         // get at least 6 bytes
-        if(ERR_OK != (err=read_len_bytes(state.conn, state.p, 6))) {
+        // get at least 6 bytes
+        if(ERR_OK != (err = read_len_bytes(state.conn, state.p, 6))) {
             return err;
         }
     }
@@ -208,58 +205,66 @@ static err_t websocket_read(WebsocketState& state, uint8_t *buf, uint16_t buflen
     uint16_t n = pbuf_copy_partial(state.p, hdr, 6, 0);
     if(n != 6) {
         printf("websocket_read: pbuf_copy_partial failed\n");
-            return ERR_VAL;
+        return ERR_VAL;
     }
     if((hdr[0] & 0x80) == 0) {
         // must have fin bit
+        // TODO need to handle this correctly, as it would notify the end of a large file upload
         printf("websocket_read: WARNING FIN bit not set\n");
+        state.complete = false;
         //return ERR_VAL;
+    } else {
+        state.complete = true;
     }
+
     if((hdr[1] & 0x80) == 0) {
         // must have mask bit
         printf("websocket_read: MASK bit not set\n");
         return ERR_VAL;
     }
     uint16_t o, m;
-    uint16_t plen= hdr[1] & 0x7F;
+    uint16_t plen = hdr[1] & 0x7F;
     if(plen < 126) {
-        o= 6;
-        m= 2;
+        o = 6;
+        m = 2;
     } else if(plen == 126) {
-        plen= (hdr[2]<<8) | (hdr[3]&0xFF);
-        o= 8;
-        m= 4;
+        plen = (hdr[2] << 8) | (hdr[3] & 0xFF);
+        o = 8;
+        m = 4;
     } else {
         uint32_t s1, s2;
         uint8_t hdr2[10];
         pbuf_copy_partial(state.p, hdr2, 10, 0);
-        s1= ((uint32_t)hdr2[2]<<24) | ((uint32_t)hdr2[3]<<16) | ((uint32_t)hdr2[4]<<8) | ((uint32_t)hdr2[5]&0xFF);
-        s2= ((uint32_t)hdr2[6]<<24) | ((uint32_t)hdr2[7]<<16) | ((uint32_t)hdr2[8]<<8) | ((uint32_t)hdr2[9]&0xFF);
+        s1 = ((uint32_t)hdr2[2] << 24) | ((uint32_t)hdr2[3] << 16) | ((uint32_t)hdr2[4] << 8) | ((uint32_t)hdr2[5] & 0xFF);
+        s2 = ((uint32_t)hdr2[6] << 24) | ((uint32_t)hdr2[7] << 16) | ((uint32_t)hdr2[8] << 8) | ((uint32_t)hdr2[9] & 0xFF);
         printf("websocket_read: unsupported length: %d - %08lX %08lX\n", plen, s1, s2);
         return ERR_VAL;
     }
 
     // FIXME we really do not want to have to load the entire packet into memory/pbufs
+
+    // check buffer size for payload before trying to read it all
+    if(buflen < plen) {
+        // the buffer provided is not big enough, tell caller what size it needs to be
+        printf("websocket_read: provided buffer (%d) is not big enough for %d bytes\n", buflen, plen);
+        readlen = plen;
+        return ERR_BUF;
+    }
+
     // make sure we have all data
-    if(ERR_OK == (err=read_len_bytes(state.conn, state.p, plen+o))) {
+    if(ERR_OK == (err = read_len_bytes(state.conn, state.p, plen + o))) {
         // read entire set of header bytes into hdr
         n = pbuf_copy_partial(state.p, hdr, o, 0);
-        // check buffer size for payload
-        if(buflen < plen) {
-            // the buffer provided is not big enough, tell caller what size it needs to be
-            printf("websocket_read: buffer is not big enough %d for %d bytes\n", buflen, plen);
-            readlen= plen;
-            return ERR_BUF;
-        }
 
-        // read entire payload into buffer
+        // read entire payload into provided buffer
         n = pbuf_copy_partial(state.p, buf, plen, o);
+        // free up read pbufs
+        state.p = pbuf_free_header(state.p, plen + o);
+
         if(n != plen) {
             printf("websocket_read: pbuf_copy_partial failed: %d\n", plen);
             return ERR_VAL;
         }
-        // free up read pbufs
-        state.p= pbuf_free_header(state.p, plen+o);
 
         // now unmask the data
         uint8_t opcode = hdr[0] & 0x0F;
@@ -268,7 +273,7 @@ static err_t websocket_read(WebsocketState& state, uint8_t *buf, uint16_t buflen
             case 0x01: // text
             case 0x02: // bin
                 /* unmask */
-                for (int i = 0; i < plen; i++){
+                for (int i = 0; i < plen; i++) {
                     buf[i] ^= hdr[m + i % 4];
                 }
                 readlen = plen;
@@ -282,17 +287,17 @@ static err_t websocket_read(WebsocketState& state, uint8_t *buf, uint16_t buflen
         }
         return ERR_OK;
 
-    }else{
+    } else {
         return err;
     }
 
     return ERR_VAL;
 }
 
-static int websocket_write(struct netconn *conn, const char *data, uint16_t len, uint8_t mode=0x01)
+static int websocket_write(struct netconn *conn, const char *data, uint16_t len, uint8_t mode = 0x01)
 {
     // potentially large packets should not be put on the stack
-    unsigned char *buf= (unsigned char*)malloc(len + 4);
+    unsigned char *buf = (unsigned char*)malloc(len + 4);
     if(buf == NULL) {
         printf("websocket_write: out of memory\n");
         return -1;
@@ -300,20 +305,20 @@ static int websocket_write(struct netconn *conn, const char *data, uint16_t len,
     buf[0] = 0x80 | mode; // binary/text
     int o;
     if (len < 126) {
-        o= 1;
+        o = 1;
         buf[1] = len;
-    }else if(len < 65535) {
-        o= 3;
-        buf[1]= 126;
-        buf[2]= len >> 8;
-        buf[3]= len & 0xFF;
-    }else {
+    } else if(len < 65535) {
+        o = 3;
+        buf[1] = 126;
+        buf[2] = len >> 8;
+        buf[3] = len & 0xFF;
+    } else {
         printf("websocket_write: buffer too big\n");
         free(buf);
         return -1;
     }
-    memcpy(&buf[o+1], data, len);
-    len += o+1;
+    memcpy(&buf[o + 1], data, len);
+    len += o + 1;
     err_t err = netconn_write(conn, buf, len, NETCONN_COPY);
     if(err != ERR_OK) {
         printf("websocket_write: error writing: %d\n", err);
@@ -327,7 +332,7 @@ static err_t handle_incoming_websocket(struct netconn *conn, const char *keystr)
     unsigned char encoded_key[32];
     int len = strlen(keystr);
     const char WS_GUID[] = "258EAFA5-E914-47DA-95CA-C5AB0DC85B11";
-    char key[len+sizeof(WS_GUID)];
+    char key[len + sizeof(WS_GUID)];
 
     /* Concatenate key */
     memcpy(key, keystr, len);
@@ -362,7 +367,7 @@ static err_t handle_incoming_websocket(struct netconn *conn, const char *keystr)
 }
 
 // this may need to stay around until command thread is done with it so we do not delete it until we need to create another
-static OutputStream *os= nullptr;
+static OutputStream *os = nullptr;
 static err_t handle_command(struct netconn *conn)
 {
     const u16_t bufsize = 256;
@@ -374,7 +379,7 @@ static err_t handle_command(struct netconn *conn)
     // if there is an old Outputstream delete it first
     if(os != nullptr) delete(os);
     // create the OutputStream that commands can write to
-    os= new OutputStream([conn](const char *ibuf, size_t ilen) { return websocket_write(conn, ibuf, ilen, 0x01); });
+    os = new OutputStream([conn](const char *ibuf, size_t ilen) { return websocket_write(conn, ibuf, ilen, 0x01); });
 
     uint16_t n;
     err_t err;
@@ -384,7 +389,6 @@ static err_t handle_command(struct netconn *conn)
         // we now have a decoded websocket payload in buf, that is n bytes long
         process_command_buffer(n, buf, os, line, cnt, discard);
     }
-    if(state.p != NULL) pbuf_free(state.p);
 
     // make sure command thread does not try to write to the soon to be closed (and deleted) conn
     os->set_closed();
@@ -402,25 +406,24 @@ static err_t handle_upload(struct netconn *conn)
 {
     err_t err;
     WebsocketState state(conn);
-    const uint16_t buflen= 2000;
-    uint8_t *buf= (uint8_t *)malloc(buflen);
+    const uint16_t buflen = 2000;
+    uint8_t *buf = (uint8_t *)malloc(buflen);
     if(buf == NULL) return ERR_MEM;
     uint16_t n;
     // read from connection until it closes
     while ((err = websocket_read(state, buf, buflen, n)) == ERR_OK) {
-        printf("websocket: got len %d\n", n);
-        int cnt= 0;
+        printf("websocket: got len %d, complete: %d\n", n, state.complete);
+        int cnt = 0;
         for (int i = 0; i < n; ++i) {
-            printf("%02X(%c) ", buf[i], buf[i]>' '?buf[i]:'_');
+            printf("%02X(%c) ", buf[i], buf[i] > ' ' ? buf[i] : '_');
             if(++cnt >= 8) {
                 printf("\n");
-                cnt= 0;
+                cnt = 0;
             }
         }
         printf("\n");
     }
     free(buf);
-    if(state.p != NULL) pbuf_free(state.p);
 
     // exit string
     const char ebuf[] = {0x88, 0x02, 0x03, 0xe8};
@@ -616,39 +619,39 @@ static void http_server_netconn_serve(struct netconn *conn)
 
         } else if(method == "GET") {
             if(request_target == "/") {
-                request_target= "/index.html";
+                request_target = "/index.html";
             }
-            std::string path= make_path(request_target.c_str());
+            std::string path = make_path(request_target.c_str());
             if(!path.empty()) {
                 write_header(conn, http_header_200);
                 std::string ext;
-                auto o= request_target.find_last_of('.');
+                auto o = request_target.find_last_of('.');
                 if(o != std::string::npos) {
-                    ext= request_target.substr(o);
+                    ext = request_target.substr(o);
                 }
 
                 //printf("ext: <%s>\n", ext.c_str());
 
                 if(ext == http_html) {
                     write_header(conn, http_content_type_html);
-                }else if(ext == http_css) {
+                } else if(ext == http_css) {
                     write_header(conn, http_content_type_css);
-                }else if(ext == http_jpg) {
+                } else if(ext == http_jpg) {
                     write_header(conn, http_content_type_jpg);
-                }else if(ext == http_png) {
+                } else if(ext == http_png) {
                     write_header(conn, http_content_type_png);
-                }else if(ext == http_gif) {
+                } else if(ext == http_gif) {
                     write_header(conn, http_content_type_gif);
-                }else if(ext == http_txt) {
+                } else if(ext == http_txt) {
                     write_header(conn, http_content_type_text);
-                }else if(ext == http_js) {
+                } else if(ext == http_js) {
                     write_header(conn, http_content_type_js);
-                }else{
+                } else {
                     write_header(conn, http_content_type_html);
                 }
                 write_page(conn, path.c_str());
 
-            }else{
+            } else {
                 write_header(conn, http_header_404);
             }
 
