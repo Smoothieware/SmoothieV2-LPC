7d35669e428ebbf8227f6e806e4b135d78d1e78e
Jim Morris <morris@wolfman.com>
2019-01-29 17:40:57 +0000

    minor changes

--- Firmware/src/main.cpp
+++ Firmware/src/main.cpp
@@ -883,11 +883,6 @@ static void smoothie_startup(void *)
     }
 #endif
 
-    // wait for command thread to start
-    // std::unique_lock<std::mutex> lk(m);
-    // cv.wait(lk);
-    // printf("Command thread started\n");
-
     struct mallinfo mi = mallinfo();
     printf("DEBUG: Initial: free malloc memory= %d, free sbrk memory= %d, Total free= %d\n", mi.fordblks, xPortGetFreeHeapSize() - mi.fordblks, xPortGetFreeHeapSize());
 
@@ -958,16 +953,6 @@ int main(int argc, char *argv[])
 // hooks from freeRTOS
 extern "C" void vApplicationIdleHook( void )
 {
-    /* vApplicationIdleHook() will only be called if configUSE_IDLE_HOOK is set
-    to 1 in FreeRTOSConfig.h.  It will be called on each iteration of the idle
-    task.  It is essential that code added to this hook function never attempts
-    to block in any way (for example, call xQueueReceive() with a block time
-    specified, or call vTaskDelay()).  If the application makes use of the
-    vTaskDelete() API function (as this demo application does) then it is also
-    important that vApplicationIdleHook() is permitted to return to its calling
-    function, because it is the responsibility of the idle task to clean up
-    memory allocated by the kernel to any task that has since been deleted. */
-
     static TickType_t last_time_check = xTaskGetTickCount();
     if(TICKS2MS(xTaskGetTickCount() - last_time_check) >= 300) {
         last_time_check = xTaskGetTickCount();
@@ -1033,7 +1018,9 @@ extern "C" void vApplicationMallocFailedHook( void )
     __asm("bkpt #0");
     for( ;; );
     #else
-    printf("FATAL: malloc/sbrk out of memory\n");
+    // we don't want to use any memory for this
+    // returns NULL to the caller
+    write_uart("FATAL: malloc/sbrk out of memory\n", 33);
     return;
     #endif
 }
